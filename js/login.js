23c85fc07e50437f362a57bef99a06a7b6ebc712
AziCode <111975081+Rakhimv@users.noreply.github.com>
2023-04-10 23:02:23 +0500

    Update login.js

--- js/login.js
+++ js/login.js
@@ -18,7 +18,7 @@ login.addEventListener("submit", function (event) {
     let email = email_log.value.toLowerCase();
     let password = password_log.value
 
-   aziz.get("https://wepro-cca85-default-rtdb.firebaseio.com/users.json")
+   axios.get("https://wepro-cca85-default-rtdb.firebaseio.com/users.json")
         .then((response) => {
             console.log(response);
             let users = Object.values(response.data).flatMap(obj => Object.values(obj));;
